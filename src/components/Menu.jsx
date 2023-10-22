95d98f5539ef5b3b9193f8586fc0e10b84d679ea
Main <gimenezfrancotomas96@gmail.com>
2023-10-22 12:26:44 -0300

    creacion de estilos para componente menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -21,7 +21,7 @@ const Menu = () => {
   };
 
   return (
-    <Navbar  variant="dark" expand="lg" style={{ zIndex: 40000 }}>
+    <Navbar sticky="top" variant="dark" expand="lg" style={{ zIndex: 40000 }}>
       <Container id='menuContainer'>
         <Link className="navbar-brand" to="/">
           <img className="img-home" alt="Portada Hotel" />
