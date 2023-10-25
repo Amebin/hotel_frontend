d646b6454f58d6f46fc0c408f7ba398ae33b07c2
Main <gimenezfrancotomas96@gmail.com>
2023-10-24 22:37:33 -0300

    creacion estilos footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,29 +1,37 @@
-import { Container, Row, Col, Card } from 'react-bootstrap'
-
+import { Container, Row, Col } from 'react-bootstrap'
+import { Link, } from 'react-router-dom'
+import './footer.css'
 const Footer = () => {
-    return (
-        <footer className="bg-dark text-white">
-      <Container>
-        <Row>
-          <Col md={6}>
-            <h5>Información de contacto</h5>
-            <p>Tu dirección de contacto</p>
-            <p>Tu número de teléfono</p>
-            <p>Tu dirección de correo electrónico</p>
-          </Col>
-          <Col md={6}>
-            <ul>
-              <li><a href="/">Inicio</a></li>
-              <li><a href="/nosotros">Nosotros</a></li>
-              <li><a href="/productos">Productos</a></li>
-              <li><a href="/contacto">Contacto</a></li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
+  return (
+
+    <Container className="text-center" id="footerContainer">
+      <footer className="page-footer font-small pt-4">
+      <hr></hr>
+      <Row>
+        <Col  md={6} lg={3} className='colFooter'>
+          <Link className="navbar-brand" to="/">Inicio</Link>
+        </Col>
+        <Col  md={6} lg={3} className='colFooter'>
+        <Link className="navbar-brand" to="/galleryimg">Galeria de imagenes</Link>
+          
+        </Col>
+        <Col  md={6} lg={3} className='colFooter'>
+        <Link className="navbar-brand" to="/aboutUs">Sobre nosotros</Link>
+          
+        </Col>
+        <Col  md={6} lg={3} className='colFooter'>
+        <Link className="navbar-brand" to="/contactUs">Contactanos</Link>
+        </Col>
+      </Row>
+      <Row id='rowFooter'>
+        <p className='footer-copyright '>© 2020 Copyright: Hotel Viajero</p>
+      </Row>
+
+    </footer> 
+    </Container>
+    
+
   );
 };
 
 export default Footer
-    
