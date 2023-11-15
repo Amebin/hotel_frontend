91b8682b5474eb823f280d7061fa5526ffa1d980
Main <gimenezfrancotomas96@gmail.com>
2023-11-14 23:35:26 -0300

    se modifican estilos

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,20 +1,21 @@
 import { Container, Row, Col } from 'react-bootstrap'
 import error404 from '../img/error.gif'
+import './error404.css'
 
 const Error404 = () => {
   return (
     <>
-      <Container className="container-fluid container-home">
+      <Container id='error404'>
         <Row>
-          <Col className="col-md-8 col-home">
+          <Col className='text-center' sm={12} xl={{ span: 4, offset: 1 }}>
             <Row>
-              <h1>Epa!: error 404</h1>
+              <h1>Error 404</h1>
               <h3>No se encuentra el contenido solicitado</h3>
             </Row>
           </Col>
           
-          <Col className="col-md-4 col-home">
-            <img id="error404" src={error404} alt="Error 404" />
+          <Col sm={{ span: 10, offset: 1 }}  xl={{ span: 4, offset: 1 }}>
+            <img id="giferror" src={error404} alt="Error 404" className="w-100"/>
           </Col>
         </Row>
       </Container>
